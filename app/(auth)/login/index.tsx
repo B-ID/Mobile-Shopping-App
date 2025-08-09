231dea93569f9475335f58b96fe98d8817c0e157
b-id <idowu98official@gmail.com>
2025-08-09 07:46:48 +0100

    removed static values

--- app/(auth)/login/index.tsx
+++ app/(auth)/login/index.tsx
@@ -2,35 +2,36 @@ import Canvas from "@/components/ui/canvas";
 import Container from "@/components/ui/container";
 import GapComponent from "@/components/ui/gap-component";
 import NativeSafeAreaView from "@/components/ui/native-safearea";
+import { Text } from "@/components/ui/rn-text";
+import { COLORS } from "@/constants/Colors";
 import LoginForm from "@/features/auth/components/forms/login";
+import { Link } from "expo-router";
 import React from "react";
-import {View} from "react-native";
-import {Text} from "@/components/ui/rn-text";
-import {Link} from "expo-router";
-import {COLORS} from "@/constants/Colors";
+import { View } from "react-native";
 
 const LoginScreen = () => {
-    return (
-        <NativeSafeAreaView style={{backgroundColor: COLORS.tint
-        }}>
-            <Canvas>
-                <GapComponent flex={0.4}/>
-                <View className="">
-                    <Container>
-                        <Text variant="title">Login</Text>
-                        <GapComponent height={24}/>
-                        <View className={'mb-4'}>
-                            <LoginForm/>
-                        </View>
-                        <Text variant={'small'} className={'text-center'}>Don&apos;t have an account?
-                            {" "}
-                            <Link className={'underline'} href={'/(auth)/sign-up'}>Sign Up</Link>
-                        </Text>
-                    </Container>
-                </View>
-            </Canvas>
-        </NativeSafeAreaView>
-    );
+  return (
+    <NativeSafeAreaView style={{ backgroundColor: COLORS.tint }}>
+      <Canvas>
+        <GapComponent flex={0.4} />
+        <View className="">
+          <Container>
+            <Text variant="title">Login</Text>
+            <GapComponent height={24} />
+            <View className={"mb-4"}>
+              <LoginForm />
+            </View>
+            <Text variant={"small"} className={"text-center"}>
+              Don&apos;t have an account?{" "}
+              <Link className={"underline"} href={"/(auth)/sign-up"}>
+                Sign Up
+              </Link>
+            </Text>
+          </Container>
+        </View>
+      </Canvas>
+    </NativeSafeAreaView>
+  );
 };
 
 export default LoginScreen;
